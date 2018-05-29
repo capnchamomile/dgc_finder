b89d54e05dc56827dfd85820c054c6d2a8ab9588
Dan <danielwindheim@gmail.com>
2018-05-29 12:53:41 -0700

    Implemented API request caching

--- sampleconfig.py
+++ sampleconfig.py
@@ -13,6 +13,10 @@ class Config(object):
 
     GMAPS_URL = 'https://www.google.com/maps/search/?api=1&query={lat},+{lng}'
 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     STATES = [('AL', 'Alabama'),
               ('AK', 'Alaska'),
               ('AZ', 'Arizona'),
